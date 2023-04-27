8eacd85f937e35e41b09fa0a22fc064702dd5902
Luis Daniel <ls.ortizdaniel@gmail.com>
2023-04-27 10:06:04 -0600

    todo search conected with the useState

--- src/components/TodoSearch.jsx
+++ src/components/TodoSearch.jsx
@@ -1,13 +1,19 @@
+import { useState } from 'react';
 import '../styles/TodoSearch.css'
 
+
 const TodoSearch = () => {
+
+  const [searchValue, setSearchValue] = useState('')
   const onSearchValue = (e) => { 
     console.log(e.target.value);
+    setSearchValue(e.target.value)
    }
   return (
     <input 
       className='TodoSearch' 
       placeholder="Input"
+      value={searchValue}
       onChange={onSearchValue}  
     />
   )
