df0c9e5e3c8a929533ebf09bc955353d601a849b
Luis Daniel <ls.ortizdaniel@gmail.com>
2023-05-01 20:32:04 -0600

    after some changes on styles button, background, and todo item

--- src/components/CreateTodoButton.jsx
+++ src/components/CreateTodoButton.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
+import '../styles/CreateTodoButton.css'
 const CreateTodoButton = () => {
   return (
     <button
+      className='CreateTodoButton'
       onClick={ ()=>console.log('hola') }
     >
       este es un boton
